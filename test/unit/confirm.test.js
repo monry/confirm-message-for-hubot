f78a527f3b5b50eff0a154e1930262680f0022a4
Tetsuya Mori <tetsuya.mori@kidsstar.co.jp>
2016-09-13 22:18:42 +0900

    Fix test using sinon

--- test/unit/confirm.test.js
+++ test/unit/confirm.test.js
@@ -1,21 +1,27 @@
 /* global before, beforeEach, after, afterEach, it, describe */
 'use strict';
 import assert from 'assert';
+import sinon from 'sinon';
 import Confirm from '../../dist/confirm.js';
 
 // Tests may also be written in ES6
 describe('Test Suite', () => {
-	var confirm;
+  var confirm;
+  var robot;
 
-	beforeEach(() => {
-		confirm = new Confirm();
-	});
+  beforeEach(() => {
+    robot = {
+      respond: sinon.spy(),
+      hear: sinon.spy(),
+    };
+    confirm = new Confirm(robot);
+  });
 
-	it('should load app', function(){
-		var actual = confirm.name;
-		var expected = 'confirm-message-for-Hubot';
+  it('should load app', function(){
+    var actual = confirm.name;
+    var expected = 'confirm-message-for-Hubot';
 
-		assert(confirm);
-		assert.equal(actual, expected);
-	});
+    assert(confirm);
+    assert.equal(actual, expected);
+  });
 });
